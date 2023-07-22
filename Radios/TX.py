e65384eaec96cdcf49261cbe4e4c2397e70c1fe7
Connor <connorKirkpatrick3@gmail.com>
2023-07-22 12:33:07 +0100

    Altered RX packet handler to hopefully pass messages to the QGC local port. Also forwarding all local messages from TX to RX

--- Radios/TX.py
+++ Radios/TX.py
@@ -1,10 +1,26 @@
-import asyncio
+import asyncio, socket
 
-from scapy.all import sendp
+from scapy.all import sendp, sniff
 from scapy.layers.dot11 import Dot11, Dot11QoS, RadioTap
 from scapy.layers.inet import UDP, IP
 from scapy.layers.l2 import LLC, SNAP
 from scapy.packet import Raw
+
+from messageStore import messageStore
+
+RX_Bridge = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+RX_IP = "127.0.0.1"
+RX_PORT = 14550
+
+
+async def packageHandler(pkt):
+    if pkt.haslayer(IP) and pkt[IP].src == "127.0.0.1":
+        print("Test Pass")
+        print("Data: ")
+        print(pkt[Raw].load)
+        RX_Bridge.sendto(pkt[Raw].load, (RX_IP, RX_PORT))
+    else:
+        print("Test Fail")
 
 
 class TX_Radio:
@@ -13,10 +29,8 @@ class TX_Radio:
                                    addr3="00:00:00:00:00:00",
                                    type=2,
                                    subtype=8) / Dot11QoS() / LLC() / SNAP() / IP(src='127.0.0.1',
-                                                                                 dst='127.0.0.1') / UDP(
-        sport=5000, dport=5001)
-    seq = 0
-    selfID = 255
+                                                                                 dst='127.0.0.1') / \
+                UDP(sport=5000, dport=5001)
 
     def __init__(self, data_stream, interface="wlan1"):
         self.interface = interface
@@ -30,13 +44,12 @@ class TX_Radio:
                 await asyncio.sleep(0.01)
             else:
                 print(msg)
+                data = self.dataFrame / Raw(load=msg)
+                await packageHandler(data)
                 # sendp(self.dataFrame / Raw(load=msg), iface=self.interface)
 
-    def rx(self):
+    async def rx(self):
+        # QGroundControl binds to port 14550 upon start, thus forward all of our received messages to there.
+        while True:
+            sniff(iface=self.interface, prn=packageHandler, store=0)
         pass
-
-# This segment of code can be used to observe if a probe request can be sent. If this is possible it means that the
-# devices do support monitor mode with injection
-# dot11 = Dot11(addr1='ff:ff:ff:ff:ff:ff', addr2='11:11:11:11:11:66',
-# addr3='22:22:22:22:22:22') probeReq = Dot11ProbeReq() dot11elt = Dot11Elt() frame = RadioTap() / dot11 / probeReq /
-# dot11elt sendp(frame, iface=interface)
