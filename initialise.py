effa275d516c8fd0c1190b130ccada8c2a65d35d
Connor <connorKirkpatrick3@gmail.com>
2023-07-26 18:05:19 +0100

    Updated code to account for cases where non-predicatable interface naming schemes are enabled

--- initialise.py
+++ initialise.py
@@ -15,8 +15,14 @@ def initialiseWiFi(wifiAdaptor='wlan1'):
 
     deviceDetails = wifiDevice.split("ID")[1].strip()[0:9]
     deviceID, deviceAddr = deviceDetails.split(":")
-
+    # Reset the USB mode to ensure it is working
     subprocess.check_output(['sudo', 'usb_modeswitch', '-v', '0x' + deviceID, '-p', '0x' + deviceAddr, '--reset-usb'])
+    # Reset the random name to predictable
+    adapters = bToString(subprocess.check_output(['iwconfig'],stderr=subprocess.DEVNULL)).split("\n\n")
+    for adapter in adapters:
+        if adapter.__contains__("WIFI@REALTEK"):
+            subprocess.run(['ip','link','set',adapter.split(" ")[0],'name','wlan1'])
+            break
     # now set the device to monitor mode and to use channel 36
     subprocess.check_output(['sudo', 'iw', 'dev', wifiAdaptor, 'set', 'type', 'monitor'])
     subprocess.check_output(['sudo', 'ip', 'link', 'set', wifiAdaptor, 'up'])
