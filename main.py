5c22394b1d20485268c744a0d4297942a0229999
Connor <connorKirkpatrick3@gmail.com>
2023-08-03 23:53:54 +0100

    Added additional input stream global variable

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from pymavlink.dialects.v20 import common as mavlink2
-import asyncio, time, os
+import asyncio, os, queue
 from threading import *
 from Radios.TX import Radio
 from initialise import initialiseWiFi
 import messageStore
 from Mavlink import messages
+from scapy.all import sniff
+
+inputStream = queue.Queue()
+
 
 async def main():
-    #initialiseWiFi()
+    print("Started")
+    # initialiseWiFi()
     os.environ["MAVLINK20"] = '1'
     os.environ["SYS_ID"] = '1'
     outputStream = messageStore.messageStore()
-
     vehicle = mavlink2.MAVLink(outputStream, srcSystem=1, srcComponent=1)
-    TX = Radio(outputStream)
-
+    TX = Radio(outputStream, inputStream)
     # Note: file is the address/device mavlink will try to transmit on. We will route this to our
     # own structure to allow us to encrypt and broadcast the message
 
@@ -26,18 +29,16 @@ async def main():
     asyncio.create_task(messages.heartBeat(vehicle))
     asyncio.create_task(messages.GPS_Raw(vehicle))
 
-
     print("Tasks created")
-
-
 
     while True:
         await asyncio.sleep(1)
-
 
     # mav.gps_raw_int_send()
     # mav.attitude_send()
 
 
 if __name__ == '__main__':
+    print("Running")
     asyncio.run(main())
+
