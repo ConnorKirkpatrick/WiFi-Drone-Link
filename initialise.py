5782c8724ddf10c636cc62685eb48d660e6a6946
Connor <connorKirkpatrick3@gmail.com>
2023-08-15 12:20:04 +0100

    removed device renaming and reseting from startup routine

--- initialise.py
+++ initialise.py
@@ -4,7 +4,8 @@ import sys, subprocess
 def bToString(arg):
     return ''.join(map(chr, arg))
 
-
+# Current ISSUES, renaming the wifi device seems to get overridden by the system as soon as it is done by the code
+# Find a fix, or update adaptor name to the found name
 def initialiseWiFi(wifiAdaptor='wlan1'):
     deviceList = bToString(subprocess.check_output(['lsusb'])).split("\n")
     wifiDevice = ""
@@ -16,13 +17,13 @@ def initialiseWiFi(wifiAdaptor='wlan1'):
     deviceDetails = wifiDevice.split("ID")[1].strip()[0:9]
     deviceID, deviceAddr = deviceDetails.split(":")
     # Reset the USB mode to ensure it is working
-    subprocess.check_output(['sudo', 'usb_modeswitch', '-v', '0x' + deviceID, '-p', '0x' + deviceAddr, '--reset-usb'])
+    #subprocess.check_output(['sudo', 'usb_modeswitch', '-v', '0x' + deviceID, '-p', '0x' + deviceAddr, '--reset-usb'])
     # Reset the random name to predictable
-    adapters = bToString(subprocess.check_output(['iwconfig'], stderr=subprocess.DEVNULL)).split("\n\n")
-    for adapter in adapters:
-        if adapter.__contains__("WIFI@REALTEK"):
-            subprocess.run(['sudo', 'ip', 'link', 'set', adapter.split(" ")[0], 'name', 'wlan1'])
-            break
+    #adapters = bToString(subprocess.check_output(['iwconfig'], stderr=subprocess.DEVNULL)).split("\n\n")
+    #for adapter in adapters:
+    #    if adapter.__contains__("WIFI@REALTEK"):
+    #        subprocess.run(['sudo', 'ip', 'link', 'set', adapter.split(" ")[0], 'name', 'wlan1'])
+    #        break
     # now set the device to monitor mode and to use channel 36
     subprocess.check_output(['sudo', 'iw', 'dev', wifiAdaptor, 'set', 'type', 'monitor'])
     subprocess.check_output(['sudo', 'ip', 'link', 'set', wifiAdaptor, 'up'])
