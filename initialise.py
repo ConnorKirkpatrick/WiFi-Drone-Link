e107924b025ee69b68db7f0cf079eb63b7758549
Connor <connorKirkpatrick3@gmail.com>
2024-12-29 16:11:28 +0000

    re-enabled sudo commands

--- initialise.py
+++ initialise.py
@@ -26,11 +26,11 @@ def initialiseWiFi(wifiAdaptor='wlan1'):
             subprocess.run(['ip', 'link', 'set', adapter.split(" ")[0], 'name', 'wlan1'])
             break
     # now set the device to monitor mode and to use channel 36
-    print(subprocess.check_output(['ip', 'link', 'set', wifiAdaptor, 'down']))
+    print(subprocess.check_output(['sudo', 'ip', 'link', 'set', wifiAdaptor, 'down']))
     time.sleep(0.3)
-    print(subprocess.check_output(['iw', wifiAdaptor, 'set', 'monitor', 'none']))
+    print(subprocess.check_output(['sudo', 'iw', wifiAdaptor, 'set', 'monitor', 'none']))
     time.sleep(0.3)
-    print(subprocess.check_output(['ip', 'link', 'set', wifiAdaptor, 'up']))
+    print(subprocess.check_output(['sudo', 'ip', 'link', 'set', wifiAdaptor, 'up']))
     time.sleep(0.3)
-    print(subprocess.check_output(['iw', 'dev', wifiAdaptor, 'set', 'channel', '36']))
+    print(subprocess.check_output(['sudo', 'iw', 'dev', wifiAdaptor, 'set', 'channel', '36']))
     time.sleep(1)
