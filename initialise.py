58e88274b8baa56d9d65137bccad1ff3dc8f4aba
Connor <connorKirkpatrick3@gmail.com>
2024-12-30 15:27:06 +0000

    altered order of setup commands, set channel first

--- initialise.py
+++ initialise.py
@@ -27,12 +27,12 @@ def initialiseWiFi(wifiAdaptor='wlan1'):
             subprocess.run(['ip', 'link', 'set', adapter.split(" ")[0], 'name', 'wlan1'])
             break
     # now set the device to monitor mode and to use channel 36
+    subprocess.check_output(['sudo', 'iw', 'dev', wifiAdaptor, 'set', 'channel', '36'])
+    time.sleep(0.3)
     subprocess.check_output(['sudo', 'ip', 'link', 'set', wifiAdaptor, 'down'])
     time.sleep(0.3)
     subprocess.check_output(['sudo', 'iwconfig', wifiAdaptor, 'mode', 'monitor'])
     #subprocess.check_output(['sudo', 'iw', wifiAdaptor, 'set', 'monitor', 'none'])
     time.sleep(0.3)
     subprocess.check_output(['sudo', 'ip', 'link', 'set', wifiAdaptor, 'up'])
-    time.sleep(0.3)
-    subprocess.check_output(['sudo', 'iw', 'dev', wifiAdaptor, 'set', 'channel', '9'])
     time.sleep(1)
