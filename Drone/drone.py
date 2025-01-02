036b3d813ecdd4ebad64e063f18d4064ab8cb96f
Connor <connorKirkpatrick3@gmail.com>
2025-01-02 16:05:18 +0000

    more debugging

--- Drone/drone.py
+++ Drone/drone.py
@@ -86,10 +86,12 @@ class GCS(Device):
                         # this means that if we receive data such as ACK after
                         # keys are set, we can still process them
                 msg_type = int.from_bytes(msg[0:1], "big")
+                print("Message type: ", msg_type)
                 if msg_type == 0 and self._current_secret is not None:
                     # new broadcast from a drone
+                    print("Creating new client")
                     self.new_client(msg)
-                    pass
+
 
                 elif msg_type == 3 and self._current_secret is not None:
                     # handshake challenge by client, respond with ack
