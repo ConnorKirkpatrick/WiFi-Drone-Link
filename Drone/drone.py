a34dad719812831d0a906939a0907582f19b7779
Connor <connorKirkpatrick3@gmail.com>
2025-01-02 15:54:22 +0000

    Added call to the packet manager

--- Drone/drone.py
+++ Drone/drone.py
@@ -42,6 +42,7 @@ class GCS(Device):
         super().__init__(device_id, interface, channel, port, own_device)
         self.id_map = {}  # {id:[obj]}
         self.port_map = {}  # {port:[obj]}
+        asyncio.run(self.manage_incoming_packets())
 
     def encrypt(self, message):
         """
