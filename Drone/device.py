6cde8234ae1800d3fa1f3da102a9024b48115071
connor <connorkirkpatrick3@gmail.com>
2025-01-26 13:32:08 +0000

    Disabled GCS client creation to try to debug radio command issues

--- Drone/device.py
+++ Drone/device.py
@@ -208,10 +208,10 @@ class GCS(Device):
         _id = msg[3:6].decode()
         _target_key = serialization.load_ssh_public_key(msg[6:])
         _port = 5005
-        _drone = Drone(_id, "", "", _port, False)
-        self.id_map[_id] = _drone
-        self.port_map[_port] = _drone
-        _drone.set_send_queue(self._send_queue)
+        #_drone = Drone(_id, "", "", _port, False)
+        #self.id_map[_id] = _drone
+        #self.port_map[_port] = _drone
+        #_drone.set_send_queue(self._send_queue)
         print("Detected drone with ID: " + _id)
         # Got a broadcast, respond with ID, pubKey, port
         # format:
